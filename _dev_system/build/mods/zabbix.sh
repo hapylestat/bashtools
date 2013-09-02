410dc9b1159afe02c1328e173bc25b737e3ec936
Reishin <hapy.lestat@gmail.com>
2013-09-03 00:04:31 +0300

    update zabbix

--- _dev_system/build/mods/zabbix.sh
+++ _dev_system/build/mods/zabbix.sh
@@ -3,6 +3,12 @@
 APPNAME=zabbix
 APPVER=2.1.3
 
+#build server 
+$OPTIONS="--enable-server --enable-agent --with-mysql=$srvDIR/mysql/bin/mysql_config"
+
+#build client only
+#$OTIONS="--enable-agent"
+
 
 if [ ! -f $srvDOWNDIR/$APPNAME-$APPVER.tar.gz ]; then
  run "wget \"http://heanet.dl.sourceforge.net/project/$APPNAME/ZABBIX Latest Development/$APPVER/$APPNAME-$APPVER.tar.gz\" -O $srvDOWNDIR/$APPNAME-$APPVER.tar.gz" "Download source"
@@ -14,16 +20,16 @@ switch_dir $srvDOWNDIR
 run "tar -xf $srvDOWNDIR/$APPNAME-$APPVER.tar.gz" "Prepare source"
 switch_dir
 
-#run "rm -rf $srvDIR/$APPNAME && rm -rf $srvBINDIR/$APPNAME-$APPVER" "Cleanup...."
-#run "mkdir $srvBINDIR/$APPNAME-$APPVER && ln -s $srvBINDIR/$APPNAME-$APPVER $srvDIR/$APPNAME" "Make links"
+run "rm -rf $srvDIR/$APPNAME && rm -rf $srvBINDIR/$APPNAME-$APPVER" "Cleanup...."
+run "mkdir $srvBINDIR/$APPNAME-$APPVER && ln -s $srvBINDIR/$APPNAME-$APPVER $srvDIR/$APPNAME" "Make links"
 
 
-#switch_dir $srvDOWNDIR/$APPNAME-$APPVER
+switch_dir $srvDOWNDIR/$APPNAME-$APPVER
 
-#run "./configure --prefix=/usr/local --enable-network --enable-ipv6 --with-default-audio=pulse --with-cpu=x86-64" "Make configuration"
+run "./configure --prefix=$srvDIR/$APPNAME --enable-ipv6 ${OPTIONS}" "Make configuration"
 
 
-#run "make" "Compile project"
+run "make" "Compile project"
 #run "make install" "Install project"
 #switch_dir
 
